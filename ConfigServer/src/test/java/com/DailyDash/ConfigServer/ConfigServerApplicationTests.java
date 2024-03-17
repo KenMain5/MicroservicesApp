50081faafac19800d403da61401790201ef5e9db
KenMain5 <kennyize15@gmail.com>
2024-03-17 11:23:09 -0400

    initial push

---
+++ ConfigServer/src/test/java/com/DailyDash/ConfigServer/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DailyDash.ConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
